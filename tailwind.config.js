c13e9a4777f9046e3589c4a1dd5f082d8c321fb9
Hojiakbardevs <abdulhakimovhojiakbar123@gmail.com>
2024-07-27 19:21:45 +0500

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        playwrite:['Nunito', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
