08030cad15ec3c861fd626f5e409336d5ca228b6
Hojiakbardevs <abdulhakimovhojiakbar123@gmail.com>
2024-08-02 22:51:28 +0500

    change text element

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -43,6 +43,7 @@ export const Navbar = () => {
                 <div>
                   <button className="rounded-full border-solid border-2 border-gray-300 py-2 px-4 hover:bg-gray-700 hover:text-gray-100">
                     Free Trial
+                    salom dunyo
                   </button>
                 </div>
               </div>
